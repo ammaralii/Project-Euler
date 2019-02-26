eec117568bbd94d3da1df294f1d805166672ec4a
Syed Ammar Ali Shah <bedeveloper18@vantibolli.com>
2019-02-26 12:52:58 +0500

    Add java doc

--- src/sample/Problem1.java
+++ src/sample/Problem1.java
@@ -2,6 +2,11 @@ package sample;
 
 import java.math.BigInteger;
 
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
 public class Problem1 {
     //This is how most people done
     public static BigInteger getSumOFMultipleOfThreeAndFive(BigInteger limit){
